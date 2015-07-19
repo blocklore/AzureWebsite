8db1f56f1d8c39dccab43e5274f177e077698066
blocklore <skennedy365176@gmail.com>
2015-07-19 18:40:32 +1000

    Fixed pathing issue with swig.

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
+/* global process */
 var http = require('http');
 var swig  = require('swig');
 
 http.createServer(function (req, res) {
     console.log('Got request for ' + req.url);
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end(swig.renderFile('/swig/Intro.html', {
+    res.end(swig.renderFile('./swig/Intro.html', {
         pagename: 'awesome people',
         authors: ['Paul', 'Jim', 'Jane']
     }));
+    // res.end("<h1>What?</h1>");
 }).listen(process.env.PORT);
