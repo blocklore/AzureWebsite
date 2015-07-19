5a91d76d5c17d40657558ab467328acd09295743
blocklore <skennedy365176@gmail.com>
2015-07-19 18:56:58 +1000

    Updated to use express module.

--- server.js
+++ server.js
@@ -1,13 +1,30 @@
+/* global __dirname */
 /* global process */
-var http = require('http');
-var swig  = require('swig');
 
-http.createServer(function (req, res) {
-    console.log('Got request for ' + req.url);
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end(swig.renderFile('./swig/Intro.html', {
+var app = require('express')(),
+  swig = require('swig'),
+  people;
+
+// This is where all the magic happens!
+app.engine('html', swig.renderFile);
+
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+
+// Swig will cache templates for you, but you can disable
+// that and use Express's caching instead, if you like:
+app.set('view cache', false);
+// To disable Swig's cache, do the following:
+swig.setDefaults({ cache: false });
+// NOTE: You should always cache templates in a production environment.
+// Don't leave both of these to `false` in production!
+
+app.get('/', function (req, res) {
+  res.render('index', {
         pagename: 'awesome people',
-        authors: ['Paul', 'Jim', 'Jane']
-    }));
-    // res.end("<h1>What?</h1>");
-}).listen(process.env.PORT);
+        authors: ['Paul', 'Jim', 'Jane', "Lane"]
+    });
+});
+
+app.listen(process.env.PORT);
+console.log('Application Started on http://localhost:%s/', [ process.env.PORT ]);
